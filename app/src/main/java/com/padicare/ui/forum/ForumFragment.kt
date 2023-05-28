b0291af6ca2366e6c9e9f6c0b56797351fd96b58
khoirul2009 <khoirulafwan20@gmail.com>
2023-05-28 10:12:29 +0700

    update

--- app/src/main/java/com/padicare/ui/forum/ForumFragment.kt
+++ app/src/main/java/com/padicare/ui/forum/ForumFragment.kt
@@ -1,5 +1,6 @@
 package com.padicare.ui.forum
 
+import android.content.Intent
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,6 +8,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.padicare.R
+import com.padicare.databinding.FragmentForumBinding
+import com.padicare.ui.addPost.AddPostActivity
 
 class ForumFragment : Fragment() {
 
@@ -16,17 +19,32 @@ class ForumFragment : Fragment() {
 
     private lateinit var viewModel: ForumViewModel
 
+    private var _binding : FragmentForumBinding? = null
+
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_forum, container, false)
+        _binding = FragmentForumBinding.inflate(inflater, container, false)
+
+        val root = binding.root
+
+        return root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ForumViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnAddPost.setOnClickListener {
+            startActivity(Intent(activity, AddPostActivity::class.java))
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
